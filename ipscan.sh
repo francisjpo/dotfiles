1480d5d3390955ac4d29a1c4f92eef7392f2531d
Francis Pinto <github@franciscopinto.me>
2018-10-25 22:18:45 +0100

    Update ipscan.sh

--- ipscan.sh
+++ ipscan.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-internalip=$1
-sudo nmap -sn $1/24 | grep -v 'Host' | tail -n +2 | tr "\n" " " | sed -e "s/MAC Address: //g" | sed -E $'s/Nmap scan report for /\\\n/g'
+for NUMBER in $(seq 0 5); do
+    ip=`ipconfig getifaddr en$NUMBER`
+    if [ -n "$ip" ]; then
+        myip="$ip"
+        break
+    fi
+done
+sudo nmap -sn $myip/24 | grep -v 'Host' | tail -n +2 | tr "\n" " " | sed -e "s/MAC Address: //g" | sed -E $'s/Nmap scan report for /\\\n/g'
